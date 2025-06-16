5e5eccb5d6aa564816a0526b5dff81107811df8f
Chetan N <chethan2913@gmail.com>
2025-06-16 21:55:39 +0530

    inventory

---
+++ inventory_tracker.py
@@ -0,0 +1,37 @@
+inventory ={
+    "Mobilephones" : 50,
+    "Laptops" : 40,
+    "Smartwatch" : 25,
+    "Refrigerator" : 20
+}
+
+def add_item(item, quantity) :
+    if item not in inventory :
+        print(f"New item {item} found. Adding to the inventory")
+        inventory[item] = quantity
+    else :
+        print(f"Item {item} found in inventory. Updating quantity")
+        inventory[item] += quantity
+    print(inventory)
+
+def remove_item(item, quantity=None) :
+    if item not in inventory:
+        print(f"{item} not found in inventory")
+        return
+    if quantity is None :
+        del inventory[item]
+        print(f"{item} removed from inventory")
+    else:
+        inventory[item] -= quantity
+        if inventory[item] <= 0:
+            inventory[item]=0
+    print(inventory)
+
+def show_inventory(item=None):
+    if item is None:
+        print(inventory)
+    elif item is not None :
+        print(inventory[item])
+
+show_inventory()
+
